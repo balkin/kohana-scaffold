4a0c5dd84cc1fe82ad78dc0a7add55480cbca51e
balkin <baron@baron.su>
2011-09-12 02:25:14 +0400

    Don't touch default routing (this time works as expected)

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('scaffold', '(scaffold(/<action>((/<column>)(/<id>))))')
+Route::set('scaffold', 'scaffold(/<action>((/<column>)(/<id>)))')
 	->defaults(array(
 		'controller' => 'scaffold',
 		'action'     => 'index',
